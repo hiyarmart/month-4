6c97d8d8ffeab3b969f437d9c9ff73013d39992e
Расул Хияров <hiyarovrasul07@gmail.com>
2024-01-03 02:35:57 +0600

    hw4

--- tv_shows/urls.py
+++ tv_shows/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('show_list/', views.show_list, name='show_list'),
+    path('', views.show_list, name='show_list'),
     path('show_list/<int:id>/', views.show_detail, name='show_detail'),
     path('show_list_id/<int:id>/', views.show_id_key, name='show_id_key'),
+    path('create_show/', views.show_create_view, name='create_show'),
+    path('show_list_delete/', views.show_list_delete_view, name='show_list_delete'),
+    path('show_drop/<int:id>/delete/', views.show_drop_view, name='show_drop'),
+    path('show_list_update/', views.show_list_edit_view, name='show_list_update'),
+    path('show_update/<int:id>/update/', views.show_update, name='show_update'),
 ]
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from . import models
+from django.shortcuts import render, get_object_or_404, redirect
+from . import models, forms
+from django.urls import reverse
+from django.http import HttpResponse
+
 
 def show_list(request):
     if request.method == 'GET':
@@ -7,11 +10,13 @@ def show_list(request):
         return render(request, template_name='shows/show_list.html',
                       context={'shows': shows})
 
+
 def show_detail(request, id):
     if request.method == "GET":
         show_id = get_object_or_404(models.Show, id=id)
         return render(request, template_name='shows/show_detail.html',
                       context={'show_id': show_id})
+
 
 def show_id_key(request, id):
     if request.method == "GET":
@@ -19,5 +24,55 @@ def show_id_key(request, id):
         return render(request, template_name='shows/show_id_key.html',
                       context={'show_id_ml': show_id_ml})
 
+
+def show_create_view(request):
+    if request.method == "POST":
+        form = forms.ShowForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('show_list'))
+    else:
+        form = forms.ShowForm()
+        return render(request, template_name='crud/create_show.html',
+                      context={"form": form})
+
+
+def show_list_delete_view(request):
+    if request.method == 'GET':
+        show_delete = models.Show.objects.all()
+        return render(request, template_name='crud/delete/show_list_delete.html',
+                      context={'show_delete': show_delete})
+
+def show_drop_view(request, id):
+    show_id = get_object_or_404(models.Show, id=id)
+    show_id.delete()
+    return redirect(reverse('show_list_delete'))
+
+# ________________________
+
+def show_list_edit_view(request):
+    if request.method == 'GET':
+        show_update = models.Show.objects.all()
+        return render(request, template_name='crud/update/show_list_update.html',
+                      context={'show_update': show_update})
+
+
+
+
+def show_update(request, id):
+    show_id = get_object_or_404(models.Show, id=id)
+
+    if request.method == 'POST':
+        form = forms.ShowForm(instance=show_id, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('show_list_update'))
+    else:
+        form = forms.ShowForm(instance=show_id)
+
+    return render(request, template_name='crud/update/show_update.html', context={
+        "form": form,
+        "show_id": show_id
+    })
 
 
